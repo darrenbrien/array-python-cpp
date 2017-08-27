5f150554e1ec7c8a3fc32fed4c7201a34336c2a1
Darren Brien <darrenbrien@aol.com>
2017-08-27 19:13:25 +0000

    reformat setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 # Cython compile instructions
-
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 import numpy
 # Use python setup.py build_ext --inplace
 # to compile
 
+extension = Extension("drillpython",
+              sources=["column.pyx", "query.pyx"],  
+              #include_dirs=["/drill/contrib/native/client/src/include/drill"],
+              #libraries = ["/drill/contrib/native/client/build/libquerySubmitter.a",],
+              language="c++",
+              #extra_objects=["/drill/contrib/native/client/build/libquerySubmitter.a",]
+)
+
 setup(
-  name = "rectangleapp",
-  ext_modules = cythonize('*.pyx'),
-  include_dirs = [numpy.get_include()]
+  name = "drillpython",
+  ext_modules = [extension],
+  include_dirs = [numpy.get_include()],
+  cmdclass = {'build_ext': build_ext },
 )
