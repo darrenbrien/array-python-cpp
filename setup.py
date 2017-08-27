3111affea9bb849ebd48de4c86c1cc09a1e654ef
Darren Brien <darrenbrien@aol.com>
2017-08-27 22:38:39 +0000

    separate pyx extensions

--- setup.py
+++ setup.py
@@ -6,17 +6,23 @@ import numpy
 # Use python setup.py build_ext --inplace
 # to compile
 
-extension = Extension("drillpython",
-              sources=["column.pyx", "query.pyx"],  
+query_ext = Extension("drillpython",
+              sources=["query.pyx"],  
               include_dirs=["/drill/contrib/native/client/src/include/","/usr/local/drill_boost_1_60_0/"],
-              #libraries = ["/drill/contrib/native/client/build/libquerySubmitter.a",],
+              language="c++",
+              extra_objects=["/drill/contrib/native/client/build/libquerySubmitter.a",]
+)
+
+column_ext = Extension("drillpython",
+              sources=["column.pyx"],  
+              include_dirs=["/drill/contrib/native/client/src/include/","/usr/local/drill_boost_1_60_0/"],
               language="c++",
               extra_objects=["/drill/contrib/native/client/build/libquerySubmitter.a",]
 )
 
 setup(
   name = "drillpython",
-  ext_modules = [extension],
+  ext_modules = [column_ext, query_ext],
   include_dirs = [numpy.get_include()],
   cmdclass = {'build_ext': build_ext },
 )
